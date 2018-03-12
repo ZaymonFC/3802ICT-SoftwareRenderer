68b5a23f0e9479d5f94a922e472dbfa58b430ecc
ZaymonFC <generation281@gmail.com>
2018-03-12 16:23:04 +1000

    Added colour member to Point class, added copy constructor to Colour class to allow variable assignment

--- LabSoftware VS2010/Colour.h
+++ LabSoftware VS2010/Colour.h
@@ -3,8 +3,8 @@ class Colour
 {
 public:
 	unsigned char r, g, b;
+	Colour();
 	Colour(unsigned char r, unsigned char g, unsigned char b);
-
+	Colour(const Colour &other);
 	Colour Interpolate(Colour colour, int steps, int index) const;
 };
-
--- LabSoftware VS2010/Point.h
+++ LabSoftware VS2010/Point.h
@@ -1,8 +1,13 @@
 #pragma once
+#include "Colour.h"
+
 class Point
 {
 public:
 	int x, y;
+	Colour colour;
+
 	Point(int x, int y);
+	Point(int x, int y, Colour colour);
 };
 
