2d7fbd849f61d33f45e37757f459a723b41dac1b
ZaymonFC <generation281@gmail.com>
2018-04-02 15:28:39 +1000

    Refactored render class to remove screen parameters by making the pointer to the screen a class memeber variable

--- LabSoftware VS2010/Render.h
+++ LabSoftware VS2010/Render.h
@@ -12,23 +12,24 @@ class Render
 public:
 	int FRAME_WIDE, FRAME_HIGH;
 	using BYTE = unsigned char;
+	BYTE * _screen;
 
-	Render(int width, int height);
+	Render(int width, int height, BYTE * screen);
 
-	void SetPixel(BYTE* screen, Point point, Colour colour) const;
+	void SetPixel(Point point, Colour colour) const;
 
-	void DrawLine_Dda(const Point p0, const Point p1, BYTE* screen) const;
+	void DrawLine_Dda(const Point p0, const Point p1) const;
 
-	void DrawClipLine(Point p1, Point p2, BYTE* screen) const;
+	void DrawClipLine(Point p1, Point p2) const;
 
 	void DrawScanLine(const int y, const Point pa, const Point pb, const Point pc, const Point pd, const Colour& leftColour,
-	                  const Colour& rightColour, BYTE* screen) const;
+	                  const Colour& rightColour) const;
 
-	void DrawTriangle(Point p1, Point p2, Point p3, BYTE* screen) const;
+	void DrawTriangle(Point p1, Point p2, Point p3) const;
 
-	void DrawPolygon(const std::vector<Point>& points, BYTE* screen) const;
+	void DrawPolygon(const std::vector<Point>& points) const;
 
-	void DrawTriangle(Face face, BYTE * screen) const;
+	void DrawTriangle(Face face) const;
 
 	~Render() = default;
 };
