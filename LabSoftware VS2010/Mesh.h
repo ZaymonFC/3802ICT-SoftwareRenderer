2833782f124558d8f410ed2cd7312f30da2d7102
ZaymonFC <generation281@gmail.com>
2018-04-03 19:41:55 +1000

    Created VJS JSON Reader

---
+++ LabSoftware VS2010/Mesh.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include "Point.h"
+#include "Face.h"
+
+class Mesh
+{
+public:
+	int vertexCount;
+	int polygonCount;
+	std::vector<Point> vertices;
+	std::vector<std::vector<int>> polygons;
+
+	Mesh(int vertexCount, int polygonCount, std::vector<Point> points, std::vector<std::vector<int>> polygons);
+
+	~Mesh() = default;
+};
+
